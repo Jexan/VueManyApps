5b0db5a7d27e5401d8b9b9e66f82a8b7f70a3329
Jexan <Jexan13@gmail.com>
2018-02-16 17:47:11 -0400

    Made the game stop when a player wins.
    - After every turn the code checks the boards for any winning
    combination.
    - When someone wins no one can longer play and a text shows who won.

--- js/app.js
+++ js/app.js
@@ -4,19 +4,66 @@ const TicTacVue = new Vue({
     data: {
         board: [[null, null, null], [null, null, null], [null, null, null]],
         turnSymbol: 'X',
-        rowsAndCols: [0,1,2]
+        rowsAndCols: [0,1,2],
+        winnerSymbol: null,
+        gameFinished: false,
     },
 
     methods: {
         putPieceAt: function(row, col) {
-            if(this.board[row][col]) {
+            if(this.board[row][col] || this.gameFinished) {
                 // Perhaps change some text in the page
                 return console.log('Error');
             }
 
             this.board[row][col] = this.turnSymbol;
 
-            this.changeTurn(); 
+            if(this.someoneWon()) {
+                this.gameFinished = true;
+            } else {
+                this.changeTurn(); 
+            }
+        },
+
+        someoneWon: function() {
+            // Horizontal
+            for (let row in this.board) {
+                let firstItem = row[0];
+
+                if (firstItem && 
+                    firstItem === row[1] &&
+                    firstItem === row[2]) {
+
+                    this.winnerSymbol = firstItem;
+                    return true;
+                }           
+            }
+
+            // Vertical
+            for (let colN in this.rowsAndCols) {
+                let firstItem = this.board[0][colN];
+             
+                if (firstItem &&
+                    firstItem === this.board[1][colN] && 
+                    firstItem === this.board[2][colN]) {
+
+                    this.winnerSymbol = firstItem;
+                    return true;
+                }
+            }
+
+            // Diagonals
+            let centerLetter = this.board[1][1];
+
+            if(centerLetter) {
+                if((centerLetter === this.board[0][0] && centerLetter === this.board[2][2]) || 
+                   (centerLetter === this.board[2][0] && centerLetter === this.board[0][2])) {
+                    this.winnerSymbol = centerLetter;
+                    return true;
+                }
+            }
+
+            return false;
         },
 
         changeTurn: function() {
