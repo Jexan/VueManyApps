1e9cbfcc4ace75b554eb3ed1086fe3e1d83283b5
Jexan <Jexan13@gmail.com>
2018-02-16 16:49:27 -0400

    Constructed the board and added basic functionality to it.
    Main
    - Added the script and style to the page.
    - Made it possible to add the letters to the squares.
    - Added the turn text.
    
    Bugs
    - Fixed a small oversight in the click binding of the app.
    - Renamed some variables and elements.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,24 @@ const TicTacVue = new Vue({
 
     data: {
         board: [[null, null, null], [null, null, null], [null, null, null]],
-        turn: 'X',
+        turnSymbol: 'X',
+        rowsAndCols: [0,1,2]
     },
+
+    methods: {
+        putPieceAt: function(row, col) {
+            if(this.board[row][col]) {
+                // Perhaps change some text in the page
+                return console.log('Error');
+            }
+
+            this.board[row][col] = this.turnSymbol;
+
+            this.changeTurn(); 
+        },
+
+        changeTurn: function() {
+            this.turnSymbol = (this.turnSymbol === 'X')? 'O' : 'X';
+        }
+    }
 });
